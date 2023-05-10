26fb93be778ac213e76ad841e2f19f50afd7523c
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-05-10 22:14:12 +0200

    add creat and verify token

---
+++ src/utils/tokens.js
@@ -0,0 +1,18 @@
+const { sign, verify } = require('jsonwebtoken')
+
+const tokenKey = process.env.TOKEN_SECRET
+
+const newToken = (userID = "") => sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+    data: userID
+}, tokenKey);
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    verify(token, tokenKey, (err) => {
+        if (err) return res.status(401);
+        next();
+    })
+}
+
+module.exports = { newToken, verifyToken };
