81804d108a16757dc16e65bb078f2336b8f0167c
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-05-10 22:15:25 +0200

    Add new route groups

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const cors = require("cors");
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { db } = require("./src/utils/database");
+const { newToken } = require("./src/utils/tokens");
+
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -13,6 +16,16 @@ app.use([
     express.urlencoded({ extended: true })
 ]);
 
+//Connect to Db
+// db.getConnection((err)=>{
+//     if(err) throw err;
+//     console.log("Database Connected");
+// })
+
+//Routes here
+console.log(newToken("8371154e-36b8-43fe-916e-2645a9774d26"));
+
+//Satrt the server
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
