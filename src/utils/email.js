becde1fe8de88bcd29764d02e6ac9bd6e4b6c791
Axolem <axolemaranjana4@gmail.com>
2023-05-12 20:27:12 +0200

    add mailing functionality

---
+++ src/utils/email.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+async function sendEmail(reciever = "", subject = "New massage", massage = "", emailAlias = "no-reply") {
+    
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_SERVER,
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    try {
+        await transporter.sendMail({
+            from: `"Miner Tracker Software"<${emailAlias}@mts.co.za>`,
+            to: reciever,
+            subject,
+            html: massage
+        });
+
+        return true
+
+    } catch (error) {
+
+        process.env.IS_DEV === "true" && console.log(error);
+        return false
+
+    }
+}
+
+module.exports = sendEmail
