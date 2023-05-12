e4f99c4fc9af987fd4b312fe14079e6e5400f65e
Axolem <axolemaranjana4@gmail.com>
2023-05-12 20:27:41 +0200

    generate password function

---
+++ src/utils/password.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+
+const getNewPassword = () => {
+    const length = 16;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$&?"';
+    const alphabetLength = alphabet.length;
+    let newPassword = '';
+    for (let i = 0; i < length; i++) {
+        newPassword += alphabet.charAt(Math.floor(Math.random() * alphabetLength));
+    }
+    return newPassword;
+}
+
+const hashPassword = (password) => {
+    return bcrypt.hashSync(password, saltRounds);
+}
+
+const verifyPassword = (password, dbPassword) => {
+    return bcrypt.compareSync(password, dbPassword);
+}
+
+module.exports = { getNewPassword, hashPassword, verifyPassword }
