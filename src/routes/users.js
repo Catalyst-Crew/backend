7ae5a75fa588c433a8af7923a3c31fcab48fb1c8
KhumbulaniMbele12 <khumbulanim30@gmail.com>
2023-05-18 14:06:37 +0200

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,21 +1,19 @@
-
 const { Router } = require('express')
 const expressAsyncHandler = require('express-async-handler')
 const { check, validationResult } = require("express-validator")
 const { db, getTimestamp } = require('../utils/database')
 
-const route = Router();
-
+const router = Router();
 
 //route to chang user acces status
-route.post('/access',
-    check(["userId", "status", "user"]),
+router.post('/access',
+    check(["userId", "status", "user"]).notEmpty(),
     (req, res, next) => {
-        const result = validationResult(req);
-        if (!result.isEmpty()) {
-            return res.send({ massage: "Missing or invalid fields", data: result.array() });
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(401).json({ errors: errors.array() });
         }
-        next();
+        next()
     },
     expressAsyncHandler(async (req, res) => {
         const { userId, status, user } = req.body;
@@ -31,19 +29,103 @@ route.post('/access',
                 id = ?;`,
             [user, getTimestamp(), status, userId],
             (err, dbResults) => {
-                if (err){
+                if (err) {
                     return res.status(500).json({ error: process.env.IS_DEV === "true" ? err : 01 });
                 }
 
-                if (dbResults.affectedRows){
+                if (dbResults.
+                    affectedRows) {
                     return res.status(200).json({ massage: "User access status updated" });
                 }
-            })
-
-
-    })
+            }
+        )
+    }
+    )
 )
 
+//route to chang user access level
+router.post('/level',
+    check(["userId", "status", "user"]).notEmpty(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(401).json({ errors: errors.array() });
+        }
+        next()
+    },
+    expressAsyncHandler(async (req, res) => {
+        const { userId, status, user } = req.body;
+
+        db.execute(`
+        UPDATE
+            users
+            SET
+
+            role = ?,
+            
+            updated_by = ?,
+            last_updated = ?,
+            created = ?,
+            access = ?,
+            WHERE
+            id = ?;`,
+            [user, getTimestamp(), status, userId],
+            (err, dbResults) => {
+                if (err) {
+                    return res.status(500).json({ error: process.env.IS_DEV === "true" ? err : 01 });
+                }
+
+                if (dbResults.
+                    affectedRows) {
+                    return res.status(200).json({ massage: "User access level status updated" });
+                }
+            }
+        )
+    }
+    )
+)
+
+/*route to delete user
+router.delete('/:id',check(["userId", "status"]).notEmpty(),
+(req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(401).json({ errors: errors.array() });
+    }
+    next()
+},
+expressAsyncHandler(async (req, res) => {
+    const { userId, status} = req.body;
+
+    db.execute(
+        DELETE
+        name = ?,
+        role = ?,
+        email = ?,
+        password = ?,
+        created_by = ?,
+        updated_by = ?,
+        last_updated = ?,
+        created = ?,
+        access = ?,
+        areasid = ?
+        FROM
+        id=?
+        [user, getTimestamp(), status, userId],
+        (err, dbResults) => {
+            if (err) {
+                return res.status(500).json({ error: process.env.IS_DEV === "true" ? err : 01 });
+            }
+
+            if (dbResults.
+                affectedRows) {
+                return res.status(200).json({ massage: "User access status updated" });
+            }
+        }
+    )
+}
+)
+)*/
 
 
 
@@ -51,11 +133,4 @@ route.post('/access',
 
 
 
-
-
-
-
-
-
-
-module.exports = route;
+module.exports = router
