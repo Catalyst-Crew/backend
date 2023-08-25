e8d4227acf280e7c51dc0c3f118a7a958b8e312b
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-08-25 05:51:06 +0200

    Get, Upload, Dounload reports complete

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -1,12 +1,13 @@
+const fs = require('fs');
 const path = require('path');
 const { Router } = require('express');
 const { check, matchedData } = require("express-validator");
 const expressAsyncHandler = require('express-async-handler');
 const createObjectCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const { verifyToken } = require('../utils/tokens');
-const { addLogToQueue } = require('../utils/logs');
 const { db } = require('../utils/database');
+const { verifyToken } = require('../utils/tokens');
+const { addLogToQueue, addReportToQueue } = require('../utils/logs');
 const { validationErrorMiddleware } = require('../utils/middlewares');
 
 const ENV = process.env.IS_DEV === "true";
@@ -24,7 +25,131 @@ router.use(expressAsyncHandler(async (req, res, next) => {
 }
 ))
 
-router.get("/", generateMeasurments)
+router.get("/:file_name",
+  check("file_name").exists().withMessage("File name is required."),
+  validationErrorMiddleware,
+  expressAsyncHandler(async (req, res) => {
+    const { file_name } = matchedData(req);
+
+    const filePath = path.join(__dirname, `../docs/${file_name}`);
+
+    if (!fs.existsSync(filePath)) {
+      res.setHeader('Content-Type', 'application/json');
+      return res.status(404).json({ error: 1, message: "File not found." });
+    }
+
+    res.setHeader('Content-Disposition', `attachment; filename=${file_name}`);
+    res.setHeader('Content-Type', 'blob');
+
+    res.download(filePath, (err) => {
+      if (err) {
+        return res.status(500).json({ error: ENV ? err : 1, message: "Failed to download the file." });
+      }
+    })
+  })
+)
+
+router.get("/",
+  expressAsyncHandler(async (_, res) => {
+    db.execute(`
+      SELECT
+        r.id, r.user_id, r.file_name, r.created_at, u.name
+      FROM
+        reports r
+      INNER JOIN
+        users u ON r.user_id = u.id
+      ORDER BY
+        created_at DESC
+      LIMIT 100
+      ;`,
+      [],
+      (err, dbResults) => {
+        if (err) {
+          return res.status(500).json({ error: ENV ? err : 1 });
+        }
+
+        if (dbResults.length === 0) {
+          return res.status(202).json({ message: "No reports found." });
+        }
+
+        res.status(200).json(dbResults)
+      }
+    )
+  })
+)
+
+router.post("/generate",
+  [
+    check("user_id").exists().withMessage("User ID is required.").toInt(),
+    check("notifty_user").exists().withMessage("Notify user is required.").isBoolean().withMessage("Notify user must be a boolean.").toBoolean(),
+    check("date_range").isArray().withMessage("Date range must be an array.").isLength({ min: 2, max: 2 }).withMessage("Date range must have 2 values."),
+    check("report_type").isIn(["measurements", "access_points", 'miners', 'sensors', 'users', 'reports', 'logs'])
+      .withMessage("Report type must be either measurements or access_points. If you want to generate a report for all the tables, use the all keyword.")
+  ],
+  validationErrorMiddleware,
+  expressAsyncHandler(async (req, res) => {
+    const { user_id, notifty_user, date_range, report_type } = matchedData(req);
+
+    const notifty_email = ""
+
+    if (notifty_user) {
+      const dbResults = db.execute(`SELECT email FROM users WHERE id = ?;`, [user_id])
+
+      dbResults.then(([dbResults, _]) => {
+        if (dbResults.length === 0) {
+          return res.status(404).json({ message: "User not found." });
+        }
+
+        const { email } = dbResults[0];
+        notifty_email = email;
+
+        addLogToQueue(user_id, "Reports", `Generating report for user ${email}.`)
+      })
+    }
+
+    const fromDate = new Date(date_range[0]);
+    const toDate = new Date(date_range[1]);
+
+    //add to queue for generating the report
+
+    //addToGenerateQueue(user_id, notifty_email, fromDate, toDate, report_type)
+
+    res.status(200).json({ message: "Generating report." })
+  })
+)
+
+router.post("/upload",
+  [
+    check("user_id").exists().withMessage("User ID is required.").toInt(),
+    check("file_name").exists().withMessage("File name is required.").escape(),
+    check("file").exists().withMessage("File is required.").escape(),
+  ],
+  validationErrorMiddleware,
+  expressAsyncHandler(async (req, res) => {
+    const { user_id, file_name, file } = matchedData(req);
+
+    const newFileName = `${file_name + Date.now()}.csv`;
+
+    try {
+      const filePath = path.join(__dirname, `../docs/${newFileName}`);
+      const base64Data = req.body.file;
+
+      const dataWithoutPrefix = base64Data.replace(/^data:\w+\/\w+;base64,/, '');
+      const buffer = Buffer.from(dataWithoutPrefix, 'base64');
+
+      fs.writeFileSync(filePath, buffer);
+
+      addLogToQueue(user_id, "Reports", `New report uploaded with name ${file_name}.`)
+      addReportToQueue(user_id, newFileName)
+
+      res.status(200).json({ message: 'File saved successfully.', file_path: filePath });
+
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred while saving the file.' });
+    }
+  })
+)
 
 function generateMeasurments() {
   const currentDate = new Date("2023-08-14T08:15:52.000Z");
