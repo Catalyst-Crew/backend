145eacc37310816ed9b12e060334c745bd745a7d
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-11 19:36:29 +0200

    Add alets

--- src/routes/measurements.js
+++ src/routes/measurements.js
@@ -14,38 +14,88 @@ router.get('/iot-data',
     check("Id").escape(),
     check("Data").escape()
   ],
-  validationErrorMiddleware
-  , expressAsyncHandler((req, res) => {
-    const { Id, Pass, Data } = matchedData(req);
+  validationErrorMiddleware,
+  expressAsyncHandler(
+    (req, res) => {
+      const { Id, Pass, Data } = matchedData(req);
 
-    const aps = [1_000_000, 1_000_001, 1_000_002];
-    const sens = [1_000_000, 1_000_001, 1_000_002];
+      const data = JSON.parse(Data);
 
-    const r1 = Math.floor(Math.random() * aps.length);
-    const r2 = Math.floor(Math.random() * sens.length);
-
-    db.execute(`
-      INSERT INTO measurements( 
-        sensor_id, 
-        access_point_id, 
-        location, 
-        other_data
-      )VALUES(
-        ?, 
-        ?, 
-        ?, 
-        ?
-      );
-      `,
-      [sens[r2], aps[r1], "Shaft-A1", JSON.stringify({ strength: 56 })],
-      (err, _) => {
-        if (err) {
-          //Add do retry queue
-          return
-        }
-
-        const query =
+      if (data[0]) {
+        db.execute(
           `
+          SELECT
+            s.id AS sensor_id,    
+            ap.id AS access_point_id,
+            ap.name AS access_point_name,
+            
+            a.id AS area_id,
+            
+            m.id AS measurement_id,
+            m.other_data AS measurement_other_data,
+            
+            min.name AS miner_name
+          FROM sensors s
+
+          LEFT JOIN (
+              SELECT
+                  m1.sensor_id,
+                  MAX(m1.created_at) AS max_created_at
+              FROM measurements m1
+              GROUP BY m1.sensor_id
+          ) latest_measurement ON s.id = latest_measurement.sensor_id
+
+          LEFT JOIN measurements m ON latest_measurement.sensor_id = m.sensor_id AND latest_measurement.max_created_at = m.created_at
+
+          LEFT JOIN access_points ap ON m.access_point_id = ap.id
+          LEFT JOIN areas a ON ap.area_id = a.id
+          LEFT JOIN miners min ON min.sensor_id = s.id
+          WHERE s.device_id = ?;`,
+          [Id],
+          (err, dbResults) => {
+            if (!err) {
+              db.execute(`INSERT INTO sensor_alerts(sensor_id, name) VALUES (?, ?);`,
+                [dbResults[0].sensor_id, dbResults[0].miner_name],
+                (err, dbResults2) => {
+                  if (!err) {
+                    centralEmitter.emit(serverEvents.NEW_ALERT, { ...dbResults[0], alert_id: dbResults2.insertId });
+                  }
+                }
+              )
+            }
+            return res.send("Ok")
+          }
+        )
+      }
+
+      const aps = [1_000_000, 1_000_001, 1_000_002];
+      const sens = [1_000_000, 1_000_001, 1_000_002];
+
+      const r1 = Math.floor(Math.random() * aps.length);
+      const r2 = Math.floor(Math.random() * sens.length);
+
+      db.execute(`
+        INSERT INTO measurements( 
+          sensor_id, 
+          access_point_id, 
+          location, 
+          other_data
+        )VALUES(
+          ?, 
+          ?, 
+          ?, 
+          ?
+        );
+        `,
+        [sens[r2], aps[r1], "Shaft-A1", JSON.stringify({ strength: 56 })],
+        (err, _) => {
+          if (err) {
+            //Add do retry queue
+            return
+          }
+
+          const query =
+            `
             SELECT
                 JSON_ARRAYAGG(
                     JSON_OBJECT(
@@ -137,37 +187,37 @@ router.get('/iot-data',
             ) ap_json ON ar.id = ap_json.area_id;
         `;
 
-        db.execute(query, [], expressAsyncHandler((err, dbResults2) => {
-          if (err) {
-            //Add to error queue
-            return res.status(500)
-          }
+          db.execute(query, [], expressAsyncHandler((err, dbResults2) => {
+            if (err) {
+              //Add to error queue
+              return res.status(500)
+            }
 
-          const access_points = dbResults2[0].areas.map(area => {
-            return area.access_points.map(access_point => {
-              return {
-                area_id: access_point.area_id,
-                area_name: access_point.area_name,
-                device_id: access_point.device_id,
-                access_point_id: access_point.access_point_id,
-                access_point_name: access_point.access_point_name,
-                access_point_status: access_point.access_point_status,
-                access_point_latitude: access_point.access_point_latitude,
-                id_prefix_access_point: access_point.id_prefix_access_point,
-                access_point_longitude: access_point.access_point_longitude,
-                access_point_created_at: access_point.access_point_created_at,
-                measurements: access_point.measurements
-              }
-            })
-          }).flat();
+            const access_points = dbResults2[0].areas.map(area => {
+              return area.access_points.map(access_point => {
+                return {
+                  area_id: access_point.area_id,
+                  area_name: access_point.area_name,
+                  device_id: access_point.device_id,
+                  access_point_id: access_point.access_point_id,
+                  access_point_name: access_point.access_point_name,
+                  access_point_status: access_point.access_point_status,
+                  access_point_latitude: access_point.access_point_latitude,
+                  id_prefix_access_point: access_point.id_prefix_access_point,
+                  access_point_longitude: access_point.access_point_longitude,
+                  access_point_created_at: access_point.access_point_created_at,
+                  measurements: access_point.measurements
+                }
+              })
+            }).flat();
 
-          centralEmitter.emit(serverEvents.NEW_MEASUREMENT, access_points);
+            centralEmitter.emit(serverEvents.NEW_MEASUREMENT, access_points);
 
-          res.status(200).json()
-        }))
-      }
-    )
-  })
+            res.status(200).json()
+          }))
+        }
+      )
+    })
 );
 
 module.exports = router;
